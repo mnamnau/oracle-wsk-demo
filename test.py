9ba05c87b38c4f7468fd8ca1ab1eaeef8a309357
Adam Kliment <adam@netmilk.net>
2025-07-06 15:17:45 +0200

    Removed unused test.py

--- test.py
+++
@@ -1,5 +0,0 @@
-import os
-
-wallet_location = r"C:\Users\klimentt\OneDrive - AMU v Praze\Plocha\Wallet_DEMO"
-print("Exists?", os.path.exists(wallet_location))
-print("TNS file exists?", os.path.exists(os.path.join(wallet_location, "tnsnames.ora")))
